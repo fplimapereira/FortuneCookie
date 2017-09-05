0d8ce3b28dce36a9eaab6b210d6395213ae21f33
Felipe P <fp.limapereira@gmail.com>
2017-09-05 12:26:28 -0300

    Implementação da funcionalidade de som na GifView

--- app/src/main/java/com/pereira/felipe/fortunecookie/MessageActivity.java
+++ app/src/main/java/com/pereira/felipe/fortunecookie/MessageActivity.java
@@ -1,5 +1,6 @@
 package com.pereira.felipe.fortunecookie;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +12,7 @@ public class MessageActivity extends AppCompatActivity {
 
     GifView gif;
     Button acao;
+    MediaPlayer mp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,15 +21,12 @@ public class MessageActivity extends AppCompatActivity {
         gif = (GifView) findViewById(R.id.gif);
         gif.setGifResource(R.mipmap.giphy);
         gif.pause();
+        mp = MediaPlayer.create(this, R.raw.crack);
         acao = (Button) findViewById(R.id.acao);
         acao.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(gif.isPaused()){
-                    gif.play();
-                }else{
-                    gif.pause();
-                }
+                mp.start();
             }
         });
 
@@ -36,10 +35,18 @@ public class MessageActivity extends AppCompatActivity {
             public void onClick(View view) {
                 if(gif.isPaused()){
                     gif.play();
+                    mp.start();
                 }else{
                     gif.pause();
                 }
             }
         });
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        mp.release();
+        mp=null;
     }
 }
