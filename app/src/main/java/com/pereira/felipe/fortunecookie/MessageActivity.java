7b0078554d5399c86abacc0358895a651a69493f
Felipe P <fp.limapereira@gmail.com>
2017-09-05 01:40:57 -0300

    Implementação de classe para tratamento de Exceptions e teste de GifView.

--- app/src/main/java/com/pereira/felipe/fortunecookie/MessageActivity.java
+++ app/src/main/java/com/pereira/felipe/fortunecookie/MessageActivity.java
@@ -2,12 +2,44 @@ package com.pereira.felipe.fortunecookie;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.cunoraz.gifview.library.GifView;
 
 public class MessageActivity extends AppCompatActivity {
 
+    GifView gif;
+    Button acao;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
+        HandleAppCrash.deploy(this, MessageActivity.class);
+        gif = (GifView) findViewById(R.id.gif);
+        gif.setGifResource(R.mipmap.giphy);
+        gif.pause();
+        acao = (Button) findViewById(R.id.acao);
+        acao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(gif.isPaused()){
+                    gif.play();
+                }else{
+                    gif.pause();
+                }
+            }
+        });
+
+        gif.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(gif.isPaused()){
+                    gif.play();
+                }else{
+                    gif.pause();
+                }
+            }
+        });
     }
 }
